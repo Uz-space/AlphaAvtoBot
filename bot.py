f0ffb0fb4d1696df333f081ac41b0d0df3d7933f
Alpha <o53316004@gmail.com>
2026-08-27 05:24:23 +0500

    Change bot token in main function
    Updated the bot token in the main function.

--- bot.py
+++ bot.py
@@ -165,7 +165,7 @@ async def menu_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
 
 # --- Asosiy funksiya ---
 def main() -> None:
-    TOKEN = "YOUR_BOT_TOKEN_HERE"  # <-- shu yerga o'z tokeningizni kiriting
+    TOKEN = "8749302193:AAFOeDLDoimdjHSVDO728nAtsBngqncy8Uk"  # <-- shu yerga o'z tokeningizni kiriting
 
     app = ApplicationBuilder().token(TOKEN).build()
 
