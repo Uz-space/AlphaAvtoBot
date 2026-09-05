58901bb1faf2419d92a8d4f2b3a2df53a67ee69d
Alpha <o53316004@gmail.com>
2026-09-05 23:43:10 +0500

    Add button style to language selection button

--- bot.py
+++ bot.py
@@ -347,7 +347,7 @@ def build_settings_keyboard(chat_id: int) -> InlineKeyboardMarkup:
     api_key_style = ButtonStyle.SUCCESS if api_key_set else ButtonStyle.DANGER
     return InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text=t(chat_id, "btn_api_key"), callback_data="settings_api_key", style=api_key_style)],
-        [InlineKeyboardButton(text=t(chat_id, "btn_language"), callback_data="settings_language")],
+        [InlineKeyboardButton(text=t(chat_id, "btn_language"), callback_data="settings_language", style=ButtonStyle.PRIMARY)],
         [InlineKeyboardButton(
             text=t(chat_id, "btn_support"),
             url="https://t.me/alphadevlab",
